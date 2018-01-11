83a229220d71e48c89cbef6db3a23b4064c9c6ad
harapan1231 <senang.harapan@gmail.com>
2018-01-11 14:59:06 +0900

    Build a scotty web server

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Data.Monoid
+
+import Web.Scotty
+import Network.HTTP.Types
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  scotty 8080 $ do
+    get "/" $ do
+      text "Hello, world."
+
+    get "/hello/:name" $ do
+      name <- param "name"
+      text $ "Hello, " <> name <> "."
+
+    get "/redirect/to/root" $ do
+      status status302
+      setHeader "X-Foo-Bar" "bazqux"
+      redirect "/"
+
